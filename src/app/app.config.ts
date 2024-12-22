0e2bcb9d393ba0551ad2528f5a07121d0494f4e8
henrique schmidt <henriquevetterschmidt@gmail.com>
2024-12-22 18:12:22 -0300

    add firebase settings, auth and chat stores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideClientHydration } from '@angular/platform-browser';
+import { environment } from '../enviroments/enviroment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),   
+  ]
 };
---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCcxlBV-QssU4nld5vqKX_gVK1aIItN8gQ",
+        authDomain: "real-time-chat-angular-fire.firebaseapp.com",
+        projectId: "real-time-chat-angular-fire",
+        storageBucket: "real-time-chat-angular-fire.firebasestorage.app",
+        messagingSenderId: "383191447224",
+        appId: "1:383191447224:web:42f781a12746320f70e64d"
+    }
+};
